4b6cb384b69b92edab05196069ef7fb3f9e6f466
Joumanahh33 <ahmedjoumana214@gmail.com>
2026-07-20 20:24:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbxuyXbcIjiguYKH6eAgKzsNujsgnv8v8QCQWVOOM6AXygyrbBmJj_gKdJ0zl2OHHq-Z3w/exec";
+let cart = [];
 
-function sendOrder(name, phone, address, products) {
-  fetch(scriptURL, {
-    method: "POST",
-    body: JSON.stringify({
-      name: name,
-      phone: phone,
-      address: address,
-      products: products
-    })
-  })
-  .then(response => alert("تم إرسال الطلب بنجاح ✅"))
-  .catch(error => alert("حدث خطأ ❌"));
+function addToCart(name, price) {
+  cart.push({
+    name: name,
+    price: price
+  });
+
+  alert("Added to cart ✅");
+  console.log(cart);
 }
